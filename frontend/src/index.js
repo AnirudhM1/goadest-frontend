c4cf673848b63211228d2e21c8de8a309e34806e
AnirudhM1 <mukherjeeanirudh100@gmail.com>
2021-03-24 16:38:02 +0530

    fixed responsiveness

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch} from "react-router-dom";
 import Blog from './components/blog/Blog';
 import Location from './components/locations/Location';
 import Map from './components/map/Map';
@@ -10,22 +10,15 @@ import App from './App';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <div>
-        <Route path="*">
-          <Header />
-        </Route>
-        <Switch>
-          {/* <Route path='/blogs' component={Blog} /> */}
-          <Route path='/locations' component={Location} />
-          <Route path='/maps' component={Map} />
+    <React.StrictMode>
+        <Router>
+            <Switch>
+                 <Header />
+            </Switch>
+        </Router>
 
-        </Switch>
-      </div>
-    </Router>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
